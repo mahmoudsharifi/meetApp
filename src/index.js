2e05fa34b9d5878816d803245a613c7434a20e75
Mahmoud <Mahmoudsharifi@hotmail.com>
2023-06-16 02:16:35 +0500

    updated CI

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as atatus from 'atatus-spa'
+atatus.config('6e420ff6d8514e53af96a797fa4a9e4c').install()
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+atatus.notify(new Error('Test Atatus Setup'))
